45bd86758fb2737844fd297b69bdbce35f84cc39
SiberianCoffe <siberiancoffe@outlook.cl>
2023-08-10 18:09:40 -0400

    tailwindcss installation

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,28 +1,9 @@
-import { useState } from 'react';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-const Hello = () => {
-  const [dir, setdir] = useState('');
-
-  window.electron.pathDocsDir.getDocsDir('pathDocsDir').then((res) => {
-    setdir(res);
-  });
-
+export const App = () => {
   return (
-    <div>
-      <p>cafe cafe</p>
-      <p>{dir}</p>
+    <div className="flex justify-center">
+      <h1>Hello, Electron!</h1>
     </div>
   );
 };
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
-}
